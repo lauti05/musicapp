a9f79118c215fc4b2a3c2e2e0e9c53232e7c2a15
lauti <acosta.lautaromartin@gmail.com>
2024-09-14 17:54:12 -0300

    se agrego el sql

---
+++ creacion.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `db_canciones`.`cancion` (`id_cancion` INT NOT NULL AUTO_INCREMENT , `nombre` VARCHAR(100) NOT NULL , `genero` VARCHAR(100) NOT NULL , `anio` DATE NOT NULL , PRIMARY KEY (`id_cancion`)); -- Se crea la tabla cancion
+
+CREATE TABLE `db_canciones`.`artista` (`id_artista` INT NOT NULL AUTO_INCREMENT , `nombre` VARCHAR(100) NOT NULL , PRIMARY KEY (`id_cancion`)); --Se crea la tabla artista
+
+INSERT INTO `cancion` (`id_cancion`, `nombre`, `genero`, `anio`) VALUES (NULL, 'Despacio', 'Indie Rock/Pop', '2018-11-29'); -- Se agregaron datos a cancion
+
+INSERT INTO `cancion` (`id_cancion`, `nombre`, `genero`, `anio`) VALUES (NULL, 'Shdows', 'Indie Pop', '2019-09-14'); -- Se agregaron datos a cancion
+
+INSERT INTO `artista` (`id_artista`, `nombre`) VALUES (NULL, 'Roosevelt'), (NULL, 'Isla de Caras') -- Se agregaron datos a artista
