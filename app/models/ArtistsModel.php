17bfe80ec81025f49d74bfd1ed1dffb4f6c1e6d5
lauti <acosta.lautaromartin@gmail.com>
2024-10-20 20:52:01 -0300

    Squashed commit of the following:
    commit 3ca02e38b2742df2172fb72017dc158c38aca2fa
    Author: Milagros <milacallejo@gmail.com>
    Date:   Sun Oct 20 19:45:01 2024 -0300
    
        Actualizo templates
    
    commit d4b2a1306b20e5ffadd45902dffb7a7dfa267cb5
    Author: Milagros <milacallejo@gmail.com>
    Date:   Sun Oct 20 19:32:08 2024 -0300
    
        Actualizaciones - Parte B
    
    commit a272602829bf6a46f53e5780a7b1cd91c0a622be
    Merge: 517e90d 6bdd5fb
    Author: Milagros <milacallejo@gmail.com>
    Date:   Sun Oct 20 18:20:29 2024 -0300
    
        Merge branch 'main' of https://github.com/lauti05/musicapp
    
    commit 517e90df7e60dd5d30ac804a3a487345849427e1
    Author: Milagros <milacallejo@gmail.com>
    Date:   Sun Oct 20 18:20:03 2024 -0300
    
        Parte B
    
    commit 6bdd5fb989a24e8f8f195264adfb298f8ec36b47
    Author: lauti <acosta.lautaromartin@gmail.com>
    Date:   Fri Oct 18 21:03:40 2024 -0300
    
        cambio estilo botones
    
    commit 9e5fccc89983d86372ae256c00631315634b54d5
    Author: lauti <acosta.lautaromartin@gmail.com>
    Date:   Fri Oct 18 20:07:24 2024 -0300
    
        Update show-songs.phtml

--- app/models/ArtistsModel.php
+++ app/models/ArtistsModel.php
@@ -4,22 +4,32 @@ class ArtistsModel {
     public function __construct(){
         $this->db = new PDO(DSN, USERNAME, '');
     }
-    public function getArtists(): array { 
-        $query = $this->db->prepare("SELECT * FROM artist");
+    public function getArtists(){ 
+        $query = $this->db->prepare('SELECT * FROM artist');
         $query->execute();
-        return $query->fetchAll(PDO::FETCH_OBJ);
+
+        $artists = $query->fetchAll(PDO::FETCH_OBJ);
+        return $artists;
     }
-    public function getArtistById($artist): object {
-        $query = $this->db->prepare("SELECT * FROM artist WHERE artist_id = :artist"); 
-        $query->bindParam(':artist', $artist);
-        $query->execute();
-        return $query->fetch(PDO::FETCH_OBJ);
+    public function getArtistById($artist_id){
+        $query = $this->db->prepare('SELECT * FROM artist WHERE artist_id = ?'); 
+        $query->execute([$artist_id]);;
+        $artistbyid = $query->fetch(PDO::FETCH_OBJ);
+        return $artistbyid; 
     }
-    public function exists($artist){
-        $query = $this->db->prepare("SELECT EXISTS(SELECT 1 FROM artist WHERE artist_id = :artist)");
-        $query->bindParam(':artist', $artist);
-        $query->execute();
-        return (bool) $query->fetchColumn(); 
+    function insertArtist($artist_name, $artist_img){
+        $query = $this->db->prepare('INSERT INTO artist(artist_name, artist_img) VALUES (?,?)');
+        $query->execute([$artist_name, $artist_img]);
+
+        return $this->db->lastInsertId();
     }
+    public function deleteArtistById($artist_id){
+        $query = $this->db->prepare('DELETE FROM artist WHERE artist_id = ?');
+        $query->execute([$artist_id]);
+    }
+    public function updateArtist($artist_id, $artist_name, $artist_img){
+        $query = $this->db->prepare('UPDATE artist SET artist_name = ?, artist_img = ? WHERE artist_id = ?');
+        $query->execute([$artist_name, $artist_img, $artist_id]);
+    }   
 }
 ?>
